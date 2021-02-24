6b01c3967472fcafe2c9324c2e2b12ffc1067c1d
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2021-02-24 11:04:32 -0300

    testando alterações

---
+++ README.md
@@ -0,0 +1,3 @@
+# NLW4
+
+trabalho em node.js para meu desenvolvimento em back-end
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/",(request, response) => {
+
+    return response.send({ message: "Hello WORLD!" });
+
+});
+
+app.post("/",(request, response) => {
+
+    return response.json({ message: "os dados foram salvos"})
+
+})
+
+app.listen(3333, () => console.log("Server is running"));
