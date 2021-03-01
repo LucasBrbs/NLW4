0ef4a5337db310589e540c8a0830fabfb02c49a9
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2021-03-01 17:29:22 -0300

    aula2 part2

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,8 +3,10 @@ import {Request, Response} from 'express';
 
 class UserController{
     async create(request: Request, response: Response){
-        
+        const body = request.body;
+        console.log(body);
+        return response.send();
     }
 }
 
-export {UserController}
+export { UserController };
